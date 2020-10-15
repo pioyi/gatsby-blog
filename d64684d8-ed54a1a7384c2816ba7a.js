353aecaa88711ff93c812e15f03b88fe9faa2ecf
Pioyi <katiforis@outlook.com>
2020-10-15 17:17:15 +0300

    Updates

---
+++ d64684d8-ed54a1a7384c2816ba7a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{PRV2:function(a,n,t){"use strict";t.d(n,"a",(function(){return o}));var e=t("Lnxd");function o(a){return Object(e.a)({tag:"svg",attr:{viewBox:"0 0 20 20",fill:"currentColor"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z",clipRule:"evenodd"}}]})(a)}}}]);
+//# sourceMappingURL=d64684d8-ed54a1a7384c2816ba7a.js.map
